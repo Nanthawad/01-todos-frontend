5ae5512729dc522b0f54df72d2d19a23b7fe7e17
Nanthawad <5806021622132@fitm.kmutnb.ac.th>
2018-01-24 21:14:18 +0700

    long

--- src/components/VisibilityInput.vue
+++ src/components/VisibilityInput.vue
@@ -3,15 +3,19 @@
     <b-field class="is-pulled-right">
       <b-radio-button v-model="visibility"
         native-value="all">
-        <span>All</span>
+        <span>All ( {{all}})</span>
       </b-radio-button>
       <b-radio-button v-model="visibility"
         native-value="active">
-        <span>Active</span>
+        <span>Active ( {{active}} )</span>
       </b-radio-button>
       <b-radio-button v-model="visibility"
         native-value="completed">
-        <span>Completed</span>
+        <span>Completed ( {{Completed}} )</span>
+      </b-radio-button>
+      <b-radio-button v-model="visibility"
+        native-value="ClearCompleted" >
+        <span>Clear Completed</span>
       </b-radio-button>
     </b-field>
   </div>
@@ -19,9 +23,28 @@
 
 <script>
 import { store } from '@/store'
-
+import { mapGetters } from 'vuex'
 export default {
   computed: {
+    ...mapGetters([
+      'todos',
+      'visibility'
+    ]),
+    all () {
+      if (this.todos) {
+        return this.todos.length
+      }
+    },
+    active () {
+      if (this.todos) {
+        return this.todos.filter(todo => todo.completed === false).length
+      }
+    },
+    Completed () {
+      if (this.todos) {
+        return this.todos.filter(todo => todo.completed === true).length
+      }
+    },
     visibility: {
       get: function () {
         return store.state.visibility
